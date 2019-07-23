335c580c1a5edc7f66206a593092b1f4695692ae
J Frost <icefang@hotmail.com>
2019-07-22 21:24:48 -0700

    Updates

---
+++ precache-manifest.2824e2b88279347da6bfded0f9946d7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23f566c11ee2fc9bf8912662c8f7526",
+    "url": "/zodiac-click/index.html"
+  },
+  {
+    "revision": "4ad01fab15539cbfeed5",
+    "url": "/zodiac-click/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/zodiac-click/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4ad01fab15539cbfeed5",
+    "url": "/zodiac-click/static/js/main.75a5f1b6.chunk.js"
+  },
+  {
+    "revision": "dc39a45b38249549fc67",
+    "url": "/zodiac-click/static/js/runtime~main.c2ac3af7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zodiac-click/static/media/logo.5d5d9eef.svg"
+  }
+]);
